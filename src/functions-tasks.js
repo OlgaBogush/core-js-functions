48dce31fd596c448ae7a8a09738b2dec0da85ca1
OlgaBogush <bohush.olga@gmail.com>
2024-04-07 18:39:32 +0300

    add: memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -110,8 +110,16 @@ function getPolynom(a, b, c) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let a;
+  let called = false;
+  return (...args) => {
+    if (!called) {
+      called = true;
+      a = func(...args);
+    }
+    return a;
+  };
 }
 
 /**
