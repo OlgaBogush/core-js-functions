2b1ff0f526459adbe8241fd7c3ddbf8e6d744b4b
OlgaBogush <bohush.olga@gmail.com>
2025-04-13 16:48:24 +0300

    feat: add resolves to logger, partialUsingArguments, getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -143,8 +143,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function repeat() {
+    let a;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        a = i;
+      }
+    }
+    return a;
+  };
 }
 
 /**
@@ -170,8 +180,14 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const str = args.map((item) => JSON.stringify(item));
+    logFunc(`${func.name}(${str}) starts`);
+    const result = func.apply(this, args);
+    logFunc(`${func.name}(${str}) ends`);
+    return result;
+  };
 }
 
 /**
@@ -187,8 +203,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => {
+    const arr = [args1, args].flat();
+    return fn.apply(this, arr);
+  };
 }
 
 /**
@@ -208,8 +227,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let a = startFrom;
+  return function () {
+    const res = a;
+    a += 1;
+    return res;
+  };
 }
 
 module.exports = {
